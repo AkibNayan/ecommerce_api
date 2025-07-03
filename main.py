f37b8feabd985d59187768032fc528185d832df1
akibnayan <akibnayan182@gmail.com>
2025-07-04 00:03:39 +0600

    user-registration

--- main.py
+++ main.py
@@ -2,12 +2,46 @@ from fastapi import FastAPI
 from tortoise import models
 from tortoise.contrib.fastapi import register_tortoise
 from models import *
+from authentication import get_password_hash
+
+#Signals
+from tortoise.signals import post_save
+from typing import List, Optional, Type
+from tortoise import BaseDBAsyncClient
 
 app = FastAPI()
+
+
+@post_save(User)
+async def create_business(
+    sender: "Type[User]",
+    instance: User,
+    created: bool,
+    using_db: "Optional[BaseDBAsyncClient]",
+    update_fields: List[str]
+) -> None:
+    if created:
+        business_obj = await Business.create(
+            business_name = instance.username, owner = instance
+        )
+        await business_pydantic.from_tortoise_orm(business_obj)
+        #send email
+
+@app.post("/registration")
+async def user_registrations(user: user_pydanticIn):
+    user_info = user.dict(exclude_unset=True)
+    user_info["password"] = get_password_hash(user_info["password"])
+    user_obj = await User.create(**user_info)
+    new_user = await user_pydantic.from_tortoise_orm(user_obj)
+    return {
+        "status": "Ok",
+        "data": f"Hello {new_user.username}, thanks for choosing our services. Please check your email for verification and click on the link to confirm registration."
+    }
 
 @app.get("/")
 def index():
     return {"message": "Welcome to the API"}
+
 
 register_tortoise(
     app,
--- models.py
+++ models.py
@@ -11,7 +11,7 @@ class User(Model):
     email = fields.CharField(max_length=200, null=False, unique=True)
     password = fields.CharField(max_length=100, null=False)
     is_verified = fields.BooleanField(default=False)
-    join_data = fields.DatetimeField(default=datetime.utcnow)
+    join_date = fields.DatetimeField(default=datetime.utcnow)
     
 class Business(Model):
     id = fields.IntField(pk=True, index=True)
@@ -34,7 +34,7 @@ class Product(Model):
     business = fields.ForeignKeyField("models.Business", related_name="products")
     
 user_pydantic = pydantic_model_creator(User, name="User", exclude=("is_verified", ))
-user_pydanticIn = pydantic_model_creator(User, name="UserIn", exclude_readonly=True)
+user_pydanticIn = pydantic_model_creator(User, name="UserIn", exclude_readonly=True, exclude=("is_verified", "join_date"))
 user_pydanticOut = pydantic_model_creator(User, name="UserOut", exclude=("password", ))
 
 business_pydantic = pydantic_model_creator(Business, name="Business")
